5f2f47cd5c5f9e7425be45a40254f455c66400c2
David Reyes <dareyes6@espe.edu.ec>
2024-02-02 20:35:36 -0500

    pass 2nd stage

--- app/server.go
+++ app/server.go
@@ -10,17 +10,22 @@ import (
 )
 
 func main() {
-	fmt.Println("Logs from your program will appear here!")
-
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
+	listener, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println(" Failed to bind to port 4221")
 		os.Exit(1)
 	}
 
-	_, err = l.Accept()
+	fmt.Println("Server listening on port 4221")
+
+	var conn net.Conn
+	conn, err = listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+	responseOk := "HTTP/1.1 200 OK\r\n\r\n"
+
+	conn.Write([]byte(responseOk))
 }
