0407fcf0fb63b7f5b5097954f593c60bbb781e1b
David Reyes <dareyes6@espe.edu.ec>
2024-02-16 22:36:28 -0500

    stage 3

--- app/server.go
+++ app/server.go
@@ -4,10 +4,18 @@ import (
 	"fmt"
 	"net"
 	"os"
-	// Uncomment this block to pass the first stage
-	// "net"
-	// "os"
+	"strings"
 )
+
+type Request struct {
+	method      string
+	path        string
+	httpVersion string
+	host        string
+	userAgent   string
+	accept      string
+	content     string
+}
 
 func main() {
 	listener, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -18,14 +26,53 @@ func main() {
 
 	fmt.Println("Server listening on port 4221")
 
-	var conn net.Conn
-	conn, err = listener.Accept()
+	conn, err := listener.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
 
-	responseOk := "HTTP/1.1 200 OK\r\n\r\n"
+	handleConnection(conn)
+}
 
-	conn.Write([]byte(responseOk))
+func handleConnection(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	totalBytesRead, err := conn.Read(buffer)
+	if err != nil {
+		fmt.Println("Could not read request.", err.Error())
+	}
+
+	request := parseRequest(totalBytesRead, &buffer)
+
+	var response string
+
+	if request.method == "GET" {
+		if request.path == "/" || request.path == "/index.html" {
+			response = "HTTP/1.1 200 OK\r\n\r\n"
+		} else {
+			response = "HTTP/1.1 404 Not Found\r\n\r\n"
+		}
+	} else {
+		response = "HTTP/1.1 404 Not Found\r\n\r\n"
+	}
+
+	conn.Write([]byte(response))
+	conn.Close()
+}
+
+func parseRequest(totalBytesRead int, buffer *[]byte) Request {
+	requestBytes := (*buffer)[:totalBytesRead]
+	request := strings.Split(string(requestBytes), "\r\n")
+
+	first := strings.Split(request[0], " ")
+
+	return Request{
+		method:      first[0],
+		path:        first[1],
+		httpVersion: first[2],
+		host:        strings.Replace(request[1], "Host: ", "", 1),
+		userAgent:   strings.Replace(request[2], "User-Agent: ", "", 1),
+		accept:      strings.Replace(request[3], "Accept: ", "", 1),
+		content:     request[4],
+	}
 }
